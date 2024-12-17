58324083b6cf83651431680fd8ba208e7084d504
qhmpblongfirst <14441418@qq.com>
2024-12-17 08:34:59 +0800

    router自动引入modules内的module

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,12 @@ import {
   createWebHistory,
   createWebHashHistory,
 } from 'vue-router'
-import loginRouter from './modules/login'
-import dashboardRouter from './modules/dashboard'
-import planRouter from './modules/plan'
 
-export const constantRoutes = [
-  loginRouter,
-  dashboardRouter,
-  planRouter,
-]
+// 自动导入路由模块
+const modules = import.meta.glob('./modules/*.js', { eager: true })
+
+// 处理路由模块
+export const constantRoutes = Object.values(modules).map(module => module.default)
 
 const router = createRouter({
   history: createWebHashHistory(),
